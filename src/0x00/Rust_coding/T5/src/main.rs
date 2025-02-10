4213a84f963d2ed11279c3cc71aa81a2e9c65287
r2covery <r2covery@outlook.com>
2025-02-10 16:45:02 +0800

    updated: correct T5

--- src/0x00/Rust_coding/T5/src/main.rs
+++ src/0x00/Rust_coding/T5/src/main.rs
@@ -1,35 +1,25 @@
 use std::sync::atomic::{AtomicU16, Ordering};
 
+#[derive(Debug, PartialEq, Eq)]
 struct UniqueId(u16);
 
 impl UniqueId {
-    static mut NEXT_ID: u16 = 0;
-
     fn new() -> Self {
-        unsafe {
-            let id = UniqueId::NEXT_ID;
-            UniqueId::NEXT_ID = UniqueId::NEXT_ID.wrapping_add(1);
-            UniqueId(id)
-        }
+        static COUNTER: AtomicU16 = AtomicU16::new(0);
+        let id = COUNTER.fetch_add(1, Ordering::Relaxed);
+        UniqueId(id)
     }
 }
 
-impl PartialEq for UniqueId {
-    fn eq(&self, other: &Self) -> bool {
-        self.0 == other.0
-    }
+#[test]
+fn test_unique_id() {
+    let id1 = UniqueId::new();
+    let id2 = UniqueId::new();
+    assert_ne!(id1, id2);
 }
 
-impl Eq for UniqueId {}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_unique_id() {
-        let id1 = UniqueId::new();
-        let id2 = UniqueId::new();
-        assert_ne!(id1, id2);
-    }
+fn main() {
+    let id1 = UniqueId::new();
+    let id2 = UniqueId::new();
+    println!("{:?}, {:?}", id1, id2);
 }
