707fb2dd11454bab073a4d5f6558a3635db7c227
r2covery <r2covery@outlook.com>
2025-02-10 16:44:46 +0800

    updated: T3 bonus

--- src/0x00/Rust_coding/T3/src/main.rs
+++ src/0x00/Rust_coding/T3/src/main.rs
@@ -1,5 +1,37 @@
 use colored::*;
 
+use colored::Colorize;
+use log::{debug, error, info, trace, warn};
+use env_logger::Builder;
+use std::io::Write;
+// 配置日志记录器
+fn setup_logger() {
+    let mut builder = Builder::new();
+    // 自定义日志输出格式
+    builder.format(|buf, record| {
+        let level_str = match record.level() {
+            log::Level::Error => record.level().to_string().red(),
+            log::Level::Warn => record.level().to_string().yellow(),
+            log::Level::Info => record.level().to_string().green(),
+            log::Level::Debug => record.level().to_string().blue(),
+            log::Level::Trace => record.level().to_string().purple(),
+        };
+        // 获取日志消息
+        let message = record.args().to_string();
+        // 根据日志级别为消息添加颜色
+        let colored_message = match record.level() {
+            log::Level::Error => message.red(),
+            log::Level::Warn => message.yellow(),
+            log::Level::Info => message.green(),
+            log::Level::Debug => message.blue(),
+            log::Level::Trace => message.purple(),
+        };
+        // 写入格式化后的日志行
+        writeln!(buf, "[{}] - {}", level_str, colored_message)
+    });
+    // 解析默认环境变量并初始化日志记录器
+    builder.parse_default_env().init();
+}
 fn main() {
     // INFO: Hello, world!，其中 INFO: 为绿色，后续内容为白色
     println!("{}: {}", "INFO".green(), "Hello, world!".white());
@@ -17,4 +49,11 @@ fn main() {
     println!("{}", "This is a magenta text with a blue background".magenta().on_blue());
     println!("{}", "This is a bold red text".red().bold());
     println!("{}", "This is an italic green text".green().italic());
+
+    setup_logger();
+    trace!("This is a trace log.");
+    debug!("This is a debug log.");
+    info!("This is an info log.");
+    warn!("This is a warning log.");
+    error!("This is an error log.");
 }
